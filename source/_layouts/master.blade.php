952312f5246fe2a29ab22c395ff64e2124109186
Leo Do <leo@pixel-mate.com>
2026-03-07 19:59:43 +0100

    chore: Delete old favicons, build artifacts, and unused images, and introduce a single SVG favicon.

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,43 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        @viteRefresh()
-        <link rel="stylesheet" href="{{ vite('source/_assets/sass/main.scss') }}">
-        <script defer type="module" src="{{ vite('source/_assets/js/main.js') }}"></script>
-        <title>@yield('title', 'Hai Long Do - Portfolio')</title>
 
-        <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/assets/images/favicons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/favicons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/assets/images/favicons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/favicons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/favicons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/assets/images/favicons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/favicons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/images/favicons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/assets/images/favicons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicons/favicon-16x16.png">
-        <link rel="manifest" href="/assets/images/favicons/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/assets/images/favicons/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    @viteRefresh()
+    <link rel="stylesheet" href="{{ vite('source/_assets/sass/main.scss') }}">
+    <script defer type="module" src="{{ vite('source/_assets/js/main.js') }}"></script>
+    <title>@yield('title', 'Long Do - Portfolio')</title>
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111744978-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+    <link rel="icon" type="image/svg+xml" href="/assets/images/favicons/favicon.svg">
+    <link rel="apple-touch-icon" href="/assets/images/favicons/favicon.svg">
+    <meta name="theme-color" content="#2563eb">
 
-            gtag('config', 'UA-111744978-1');
-        </script>
-    </head>
-    <body class="antialiased text-gray-800 bg-gray-50 font-sans leading-normal">
-        @yield('body')
-    </body>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111744978-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-111744978-1');
+    </script>
+</head>
+
+<body class="antialiased text-gray-800 bg-gray-50 font-sans leading-normal">
+    @yield('body')
+</body>
+
 </html>
